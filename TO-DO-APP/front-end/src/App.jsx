10ff035ca8cad1b048b911dacacb0ec3faeb3626
Manvendra <welcome2github@gmail.com>
2024-01-20 13:19:03 +0530

    Front_end

--- TO-DO-APP/front-end/src/App.jsx
+++ TO-DO-APP/front-end/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { CreateToDo } from './Components/CreateToDo'
+import { ToDo } from './Components/ToDo'
 
 function App() {
 
   return (
     <>
     <div>
-      hi there 
+      <CreateToDo></CreateToDo>
+      <ToDo></ToDo>
     </div>
     </>
   )
--- TO-DO-APP/front-end/src/CreateToDo.jsx
+++
@@ -1,7 +0,0 @@
-export function CreateToDo(){
-    return <div>
-        <input type="text" placeholder="title" /> <br />
-        <input type="text" placeholder="description" /> <br />
-        <button>Add to-do +</button>
-    </div>
-}
